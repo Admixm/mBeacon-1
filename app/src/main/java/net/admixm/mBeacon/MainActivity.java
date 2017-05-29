e877b887b4468f62d883f6a71afec0091ff20a93
Usagination <tjddlf2645@gmail.com>
2017-05-29 18:09:27 +0900

    no message

--- app/src/main/java/net/admixm/mBeacon/MainActivity.java
+++ app/src/main/java/net/admixm/mBeacon/MainActivity.java
@@ -1,21 +1,21 @@
 package net.admixm.mbeacon;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import net.admixm.mbeacon.mbeaconsample.R;
+import net.admixm.mbeacon.parameters.ADMXParameters;
 import net.admixm.mbeacon.utils.ADMXLog;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    //applicationCode 는 '애드믹스엠 홀딩스'에서 지정해준 앱 넘버로 설정
-    private String applicationCode = "3";
+    private String APP_NO = "3";
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -33,16 +33,28 @@ public class MainActivity extends AppCompatActivity
         versionName.setText( ADMXBeaconAdServiceLib.SDK_VERSION );
 
         TextView appCode = ( TextView ) findViewById( R.id.text_view_app_no );
-        appCode.setText( applicationCode );
+        appCode.setText( APP_NO );
+
+        Button stopButton = ( Button ) findViewById( R.id.button_stop );
+        stopButton.setOnClickListener( new View.OnClickListener( )
+        {
+            @Override
+            public void onClick( View v )
+            {
+                ADMXBeaconAdServiceLib.dispose( MainActivity.this );
+            }
+        } );
     }
 
     private void initMBeaconSDK( )
     {
-        final ADMXParameters params = new ADMXParameters();
-        params.ApplicationCode = applicationCode;
-        params.BeaconUUIDList = new ArrayList< >( );
+        final ADMXParameters params = new ADMXParameters( );
+
+        // 애드믹스엠 홀딩스에서 발급받은 어플리케이션 코드를 기입합니다.
+        params.ApplicationCode = APP_NO;
 
         // 스캔할 비콘 uuid 리스트를 작성합니다.
+        params.BeaconUUIDList = new ArrayList<>( );
         params.BeaconUUIDList.add( "6bed2915-45e9-45fd-885a-7c648112119a" );
         params.BeaconUUIDList.add( "B9407F30-F5F8-466E-AFF9-25556B57FE6D" );
         params.BeaconUUIDList.add( "e2c56db5-dffb-48d2-b060-d0f5a71096e0" );
@@ -50,23 +62,14 @@ public class MainActivity extends AppCompatActivity
         params.BeaconUUIDList.add( "73a8edce-1227-9db7-18ff-6243e19db53d" );
         params.BeaconUUIDList.add( "c276ea6f-7de8-444a-9904-7ead820de7d9" );
 
-
-        // 광고 수신 결과를 서버로 보낼지 여부를 설정합니다.
-        // false로 설정하면 광고를 계속해서 받을 수 있습니다. 디버그 할 때만 사용해주세요.
-        ADMXBeaconAdServiceLib.setSendingAdResult( this, true );
-
-        // mBeacon 로그를 사용할지 여부를 설정합니다.
         ADMXBeaconAdServiceLib.setDebugMode( this, true );
-
-        // 화면이 꺼진 채로 광고를 받았을 때 광고를 바로 표시할지 여부를 설정합니다.
-        ADMXBeaconAdServiceLib.enableAutoDisplayOnScreenOff( this, true );
-
 
         PermissionHelper.activatePermission( this, new Runnable( )
         {
             @Override
             public void run( )
             {
+                // mBeacon SDK를 초기화합니다.
                 ADMXBeaconAdServiceLib.init( MainActivity.this, params );
             }
         } );
